e5e2def3530e4c8d697ceb51f103b2ba6029b9d4
nafisaa11 <jnafisah50@gmail.com>
2024-10-29 09:04:37 +0700

    File pertama, coba API

---
+++ application/views/welcome_message.php
@@ -0,0 +1,2 @@
+
+	<h1 class="text-lg text-red-600">Hello Eorld</h1>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./application/views/**/*.php"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
