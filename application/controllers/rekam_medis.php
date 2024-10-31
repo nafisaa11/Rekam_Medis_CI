af8b301309728fdcd1bd59970843ff15bb31c4f7
nafisaa11 <jnafisah50@gmail.com>
2024-10-31 15:01:48 +0700

    yang bener ini

--- application/controllers/rekam_medis.php
+++ application/controllers/rekam_medis.php
@@ -8,20 +8,12 @@ class Rekam_medis extends CI_Controller
         $this->load->model('RekamMedis_model'); // Nama model harus sama persis
     }
 
-    public function index()
+    public function main()
     {
         $data['judul'] = 'Halaman Utama';
-        $data['pasien'] = $this->RekamMedis_model->getAllPasien(); // Pemanggilan model yang benar
+        $data['pasien'] = $this->RekamMedis_model->getAllPasien();
         $this->load->view('template/header', $data);
         $this->load->view('Rekam_medis/main', $data);
-        $this->load->view('template/footer');
-    }
-
-    public function coba()
-    {
-        $data['judul'] = 'Coba';
-        $this->load->view('template/header', $data);
-        $this->load->view('Rekam_medis/coba');
         $this->load->view('template/footer');
     }
 }
