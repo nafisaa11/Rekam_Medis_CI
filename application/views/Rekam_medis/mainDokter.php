e3594f5a126b9f02ae4d9e23b7aed92bf20a2881
selvi937rn <selviriskanisa@gmail.com>
2024-11-07 12:22:58 +0700

    fixing button sidebar

--- application/views/Rekam_medis/mainDokter.php
+++ application/views/Rekam_medis/mainDokter.php
@@ -14,27 +14,28 @@
         </div>
         <!-- Button -->
         <div class="flex mt-8">
-            <a href="<?= base_url(); ?>Rekam_medis/main">
-                <div class="flex w-full h-12 bg-Bg3 px-4 py-2 rounded-lg items-center shadow-Button">
-                    <i class="fa-solid fa-file-medical text-black w-7 mr-1"></i>
-                    <p class="p-regular text-black">Data Pasien</p>
-                </div>
+            
+            <div class="flex w-full h-12 bg-Bg3 px-4 py-2 rounded-lg items-center shadow-Button hover:bg-Main9">
+                <a href="<?= base_url(); ?>Rekam_medis/main">
+                    <i class="fa-solid fa-file-medical text-black w-6"></i>
+                    <label class="p-regular text-black">Data Pasien</label>
+                </a>
+            </div>
             </a>
         </div>
         <div class="flex mt-5">
-            <div class="flex w-full h-12 bg-Bg3 px-4 py-2 rounded-lg items-center shadow-Button hover:bg-Main9">
-                <i class="fa-solid fa-file-medical text-black w-7 mr-1"></i>
-                <p class="p-regular text-black">Data Dokter</p>
+            <div class="flex w-full h-12 bg-Bg3 px-4 py-2 rounded-lg items-center shadow-Button">
+                <i class="fa-solid fa-file-medical text-black w-7"></i>
+                <label class="p-regular text-black">Data Dokter</label>
             </div>
         </div>
-        <div class="flex mt-5">
-            <a href="<?= base_url(); ?>Rekam_medis/TambahDokter">
-                <div
-                    class="flex w-full h-12 bg-Bg3 px-4 py-2 rounded-lg justify-center items-center shadow-Button hover:bg-Main9">
-                    <i class="fa-solid fa-plus text-black w-7 mr-1"></i>
-                    <p class="p-regular text-black">Tambah Dokter</p>
-                </div>
-            </a>
+        <div class="flex w-full mt-5">
+            <div class="flex w-full h-12 bg-Bg3 px-4 py-2 rounded-lg items-center shadow-Button hover:bg-Main9">
+                <a href="<?= base_url(); ?>Rekam_medis/TambahDokter">
+                    <i class="fa-solid fa-plus text-black w-5 mr-1"></i>
+                    <label class="p-regular text-black">Tambah Dokter</label>
+                </a>
+            </div>
         </div>
         <div class="flex absolute bottom-12 left-12">
             <div
